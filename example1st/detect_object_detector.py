474556f929b30b88ff8531e0b28ac3fac4b97958
toyoda <toyoda@localworks.jp>
2018-11-08 19:22:58 +0900

    完成

--- example1st/detect_object_detector.py
+++ example1st/detect_object_detector.py
@@ -3,7 +3,7 @@ import os
 import sys
 import glob
 import re
-
+import shutil
 import dlib
 from skimage import io
 
@@ -31,34 +31,36 @@ for f in glob.glob(os.path.join(faces_folder, "*.jpg")):
     win.set_image(img)
     win.add_overlay(dets)
 
-filename = re.search('([^/]+?)?$' , f)
-num = 0
-while num < 1:
-    print("it's correct? y/n if y, I will learn this image.")
-    answer = input('>> ')
+    filename = re.search('([^/]+?)?$' , f)
+    num = 0
+    while num < 1:
+        print("it's correct? y/n if y, I will learn this image.")
+        answer = input('>> ')
 
-    if answer == 'y':
-        datasets = []
-        for k, d in enumerate(dets):
-            datasets.append("<box top=\'{}\' left=\'{}\' width=\'{}\' height=\'{}\'/>".format(
-            d.top(), d.left(), d.right()-d.left(), d.bottom()-d.top()))
+        if answer == 'y':
+            datasets = []
+            for k, d in enumerate(dets):
+                datasets.append("<box top=\'{}\' left=\'{}\' width=\'{}\' height=\'{}\'/>".format(
+                d.top(), d.left(), d.right()-d.left(), d.bottom()-d.top()))
 
-        with open('/Users/toyoda/Desktop/read.xml') as f:
-            l = f.readlines()
-            l.insert(6,'<image file=\''+filename.group()+'\'>\n')
-            l.insert(7,'</image>\n')
-            for index, item in enumerate(datasets):
-                print("インデックス：" + str(index) + ", 値：" + item)
-                l.insert(index+7, item+'\n')
+            with open('./learning/mydataset.xml') as f:
+                l = f.readlines()
+                l.insert(6,'<image file=\''+filename.group()+'\'>\n')
+                l.insert(7,'</image>\n')
+                for index, item in enumerate(datasets):
+                    print("インデックス：" + str(index) + ", 値：" + item)
+                    l.insert(index+7, item+'\n')
 
-        with open('/Users/toyoda/Desktop/read.xml', mode='w') as f:
-            f.writelines(l)
-        num = 1
+            with open('./learning/mydataset.xml', mode='w') as f:
+                f.writelines(l)
 
-    elif answer == 'n':
-        print('ok,this image isn\'t learning.')
-        num = 1
-    else:
-        print('you should tap y or n.')
+            shutil.copyfile("./test/"+filename.group(), "./learning/"+filename.group())
+            num = 1
 
-    dlib.hit_enter_to_continue()
+        elif answer == 'n':
+            print('ok,this image isn\'t learning.')
+            num = 1
+        else:
+            print('you should tap y or n.')
+
+        dlib.hit_enter_to_continue()
